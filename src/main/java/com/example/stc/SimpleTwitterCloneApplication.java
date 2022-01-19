2a96cc27fbb2d8fd12c0bdc77e3e572353ea15f8
Konstantin Boryagin <nokss89@gmail.com>
2022-01-19 13:26:45 +0300

    connected psql and added GreetingController

---
+++ src/main/java/com/example/stc/SimpleTwitterCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.stc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTwitterCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTwitterCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stc/SimpleTwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
